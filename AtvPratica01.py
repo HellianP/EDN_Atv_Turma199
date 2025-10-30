0be71ab436989b6c17d19f210ac932288774dbda
HellianP <hellianpeixinho@hotmail.com>
2025-10-30 16:17:56 -0300

    Atividade 1

---
+++ AtvPratica01.py
@@ -0,0 +1,35 @@
+#Atividade pratica 01
+print("Atividade Prática 01")
+print("Hello, World!")
+
+#2- Calculadora de Soma
+print("Calculadora de Soma")
+numero1 = 12
+numero2 = 14
+soma = numero1 + numero2
+print("A soma de", numero1, "e", numero2, "é:", soma)
+
+#3- Calculadora de Volume
+print("Calculadora de Volume")
+comprimento = 12
+largura = 14
+altura = 20
+volume = comprimento * largura * altura
+print("O volume da caixa retangular é:", volume, "cm³")
+
+#4- Calculadora de Preço Total
+print("Calculadora de Preço Total")
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+preco_total = preco_unitario * quantidade
+print("Produto:", nome_produto, "Preço unitário: R$", preco_unitario, "Quantidade:", quantidade, "Preço total: R$", preco_total)
+
+#5- Calculadora de Número Inteiro
+print("Calculadora de Número Inteiro")
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+C = int(input("Digite o valor de C: "))
+D = int(input("Digite o valor de D: "))
+DIFERENCA = ((A * B) - (C * D))
+print("DIFERENCA =", DIFERENCA)
